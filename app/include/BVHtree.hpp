5129764e4de15a31585f2391cba5dc6e3214bf9b
Luka <lukaajdukovicc@icloud.com>
2026-07-25 20:51:32 +0200

    Enhance BVHTree implementation by adding Node and Primitive structures, and populate m_primitives with mesh data

--- app/include/BVHtree.hpp
+++ app/include/BVHtree.hpp
@@ -1,5 +1,8 @@
+#pragma once
+
 #include "engine/resources/Model.hpp"
 #include "glm/ext/vector_float3.hpp"
+#include <cstdint>
 
 namespace engine::resources {
 class BVHTree {
@@ -8,11 +11,22 @@ public:
     ~BVHTree() = default;
 
 private:
+    struct Node {
+        glm::vec3 min_bound{};
+        glm::vec3 max_bound{};
+        // > 0 leaf, == 0 inner node nno triangles
+        uint32_t left_child = 0;
+        // from where to where
+        uint32_t first_primitive = 0;
+        uint32_t primitive_count = 0;
+    };
     struct Primitive {
-
+        glm::vec3 v0, v1, v2;
         glm::vec3 centroid{};
         glm::vec3 max_bound{};
         glm::vec3 min_bound{};
     };
+
+    std::vector<Primitive> m_primitives{};
 };
 }// namespace engine::resources
--- app/src/BVHtree.cpp
+++ app/src/BVHtree.cpp
@@ -1,14 +1,38 @@
 #include "BVHtree.hpp"
 #include "engine/resources/Mesh.hpp"
 #include "engine/resources/Model.hpp"
+#include "glm/common.hpp"
+#include "glm/ext/vector_float3.hpp"
+#include <cstdint>
+
+// TODO - IMPLEMENT SAH ALGORITHM IF THERE IS TIME LEFT / IF THIS IS SUCCESSFUL
 
 namespace engine::resources {
 BVHTree::BVHTree(const Model &model) {
     const std::vector<Mesh> &meshes = model.meshes();
     for (const Mesh &mesh: meshes) {
-        // TODO implement tree building
-        const auto x = mesh;
+        const std::vector<Vertex> &vertices = mesh.vertices();
+        const std::vector<uint32_t> &indices = mesh.indices();
+        for (size_t i = 0; i + 2 < indices.size(); i += 3) {
+            uint32_t i0 = indices[i];
+            uint32_t i1 = indices[i + 1];
+            uint32_t i2 = indices[i + 2];
+
+            const glm::vec3 &p0 = vertices[i0].Position;
+            const glm::vec3 &p1 = vertices[i1].Position;
+            const glm::vec3 &p2 = vertices[i2].Position;
+
+            Primitive prim{};
+            prim.v0 = p0;
+            prim.v1 = p1;
+            prim.v2 = p2;
+            prim.min_bound = glm::min(p0, glm::min(p1, p2));
+            prim.max_bound = glm::max(p0, glm::max(p1, p2));
+            prim.centroid = (p0 + p1 + p2) / 3.0f;
+            m_primitives.push_back(prim);
+        }
     }
 }
+
 
 }// namespace engine::resources
