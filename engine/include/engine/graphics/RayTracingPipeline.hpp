15b58f793643a1793f8ac3fb57a7c19a49f179aa
Luka <lukaajdukovicc@icloud.com>
2026-07-26 00:30:56 +0200

    Refactor RayTracingPipeline: add setup_screen_quad method and integrate it into initialize

--- engine/include/engine/graphics/RayTracingPipeline.hpp
+++ engine/include/engine/graphics/RayTracingPipeline.hpp
@@ -11,6 +11,13 @@ public:
     void render();
 
 private:
+    void setup_screen_quad();
     int32_t m_max_texture_buffer_texels = 0;
+
+    uint32_t m_node_buffer = 0;
+    uint32_t m_node_texture = 0;
+
+    uint32_t m_primitive_buffer = 0;
+    uint32_t m_primitive_texture = 0;
 };
 }// namespace engine::graphics
--- engine/src/RayTracingPipeline.cpp
+++ engine/src/RayTracingPipeline.cpp
@@ -8,8 +8,15 @@
 
 namespace engine::graphics {
 void RayTracingPipeline::initialize(resources::Shader &shader) {
-    CHECKED_GL_CALL(glGetIntegerv, GL_MAX_TEXTURE_BUFFER_SIZE, &m_max_texture_buffer_texels);
-    spdlog::info("GL_MAX_TEXTURE_BUFFER_SIZE = {}", m_max_texture_buffer_texels);
+    // CHECKED_GL_CALL(glGetIntegerv, GL_MAX_TEXTURE_BUFFER_SIZE, &m_max_texture_buffer_texels);
+    // spdlog::info("GL_MAX_TEXTURE_BUFFER_SIZE = {}", m_max_texture_buffer_texels);
+    CHECKED_GL_CALL(glGenBuffers, 1, &m_node_buffer);
+    CHECKED_GL_CALL(glGenTextures, 1, &m_node_texture);
+
+    CHECKED_GL_CALL(glGenBuffers, 1, &m_primitive_buffer);
+    CHECKED_GL_CALL(glGenTextures, 1, &m_primitive_texture);
+
+    setup_screen_quad();
 }
 
 void RayTracingPipeline::upload(resources::BVHTree &tree) {
@@ -25,10 +32,25 @@ void RayTracingPipeline::upload(resources::BVHTree &tree) {
     RG_GUARANTEE(primitive_texels <= static_cast<size_t>(m_max_texture_buffer_texels),
                  "BVH primitive buffer needs {} texels but GL_MAX_TEXTURE_BUFFER_SIZE is {}.",
                  primitive_texels, m_max_texture_buffer_texels);
+
+    CHECKED_GL_CALL(glBindBuffer, GL_TEXTURE_BUFFER, m_node_buffer);
+    CHECKED_GL_CALL(glBufferData, GL_TEXTURE_BUFFER, nodes.size() * sizeof(float), nodes.data(), GL_STATIC_DRAW);
+    CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, m_node_texture);
+    CHECKED_GL_CALL(glTexBuffer, GL_TEXTURE_BUFFER, GL_RGBA32F, m_node_buffer);
+
+    CHECKED_GL_CALL(glBindBuffer, GL_TEXTURE_BUFFER, m_primitive_buffer);
+    CHECKED_GL_CALL(glBufferData, GL_TEXTURE_BUFFER, primitives.size() * sizeof(float), primitives.data(), GL_STATIC_DRAW);
+    CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, m_primitive_texture);
+    CHECKED_GL_CALL(glTexBuffer, GL_TEXTURE_BUFFER, GL_RGBA32F, m_primitive_buffer);
 }
 
 void RayTracingPipeline::render() {
     // TODO
     // camera params imgui stuff etc etc
 }
+
+void RayTracingPipeline::setup_screen_quad() {
+    // TODO
+}
+
 }// namespace engine::graphics
