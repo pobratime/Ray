4d41ac50334f562e31b091a205e48335ac43e838
pobratime <lukaajdukovicc@icloud.com>
2026-08-04 20:40:19 +0200

    Task in enqueue is now lambda instead of bind

--- engine/include/engine/util/ThreadPool.hpp
+++ engine/include/engine/util/ThreadPool.hpp
@@ -14,10 +14,9 @@
 namespace engine::util::parallel {
 class ThreadPool {
 public:
-    // dakle ako se ne navede broj, koristi onoliko koliko mozes
-    ThreadPool(size_t n_threads = std::thread::hardware_concurrency()) {
+    ThreadPool(size_t n_threads = 1) {
         for (size_t i = 0; i < n_threads; i++) {
-            // manje vise jednostavna logika enqueue je glavna komplikacija
+            m_threads.reserve(n_threads);
             m_threads.emplace_back([this] {
                 for (;;) {
                     std::unique_lock<std::mutex> lock(this->m_mutex);
@@ -34,33 +33,30 @@ public:
         }
     }
     ~ThreadPool() {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_stop = true;
-        lock.unlock();
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            m_stop = true;
+        }
         m_condition.notify_all();
         for (auto &thread: m_threads) {
             thread.join();
         }
     }
 
-    // funkija i proizvoljan broj argumenata (da bi radilo sa svime)
     template<typename F, typename... Args>
-    // mora vracati std::future u suprotnom gubimo paralelan rad, jer ce odraditi u enqueue to sto treba
-    // koristi se auto jer kompajler nema pojma o tome sta su F i Args ako stavimo pre enqueue
     auto enqueue(F &&f, Args &&...args) -> std::future<std::invoke_result_t<F, Args...>> {
         using return_type = std::invoke_result_t<F, Args...>;
-        // ovde je koriscene neke funkcionalnih stvari
-        // std::bind vraca callable objekat (f-ja je sada gradjanin prvog reda)
-        // kako moramo da koristimo future za paralelizaciju stvavimo ga u packaged_task
-        // kako nam packaged_task ne znaci puno pravimo pokazivac ka njemu
-        auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind<return_type>(std::forward<F>(f), std::forward<Args>(args)...));
-        std::future<return_type> res = task->get_future();
-        std::unique_lock<std::mutex> lock(m_mutex);
-        // pravimo void lambda f-ju
-        m_tasks.emplace([task] {
-            (*task)();
+        // auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind<return_type>(std::forward<F>(f), std::forward<Args>(args)...));
+        auto task = std::make_shared<std::packaged_task<return_type()>>([f = std::forward<F>(f), ... args = std::forward<Args>(args)] {
+            return f(args...);
         });
-        lock.unlock();
+        std::future<return_type> res = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_tasks.emplace([task] {
+                (*task)();
+            });
+        }
         m_condition.notify_one();
         return res;
     }
